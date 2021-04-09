4647f79495ff5ee9b81e90930e29d5031ee6c0a5
Arturo Bernal <arturober@gmail.com>
2021-04-09 09:58:10 +0200

    angular parte 5

--- Angular/Parte 5/angular-products/src/app/product-detail/product-detail.component.ts
+++ Angular/Parte 5/angular-products/src/app/product-detail/product-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ActivatedRoute } from '@angular/router';
+import { Producto } from '../interfaces/producto';
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'product-detail',
@@ -7,11 +10,20 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  product!: Producto;
 
-  constructor(private title: Title) { }
+  constructor(
+    private title: Title,
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) { }
 
   ngOnInit(): void {
     this.title.setTitle('Angular Products | Product detail');
+    const id = +this.route.snapshot.params.id;
+    this.productsService.getProducto(id).subscribe(
+      product => this.product = product
+    );
   }
 
 }
